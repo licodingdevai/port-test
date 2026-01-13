93a2b4486ad8973091026ba8ddf141395cc07375
kurtulus <kurtulus@kurtulus-MacBook-Air.local>
2026-01-13 19:08:59 +0300

    Change default port to 4144

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4144;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/health' && req.method === 'GET') {
